68b9732f0b5989bf37b5a787091543a99eb28768
kiki13579 <arca.sins9@gmail.com>
2025-07-15 11:12:37 +0200

    feat: Connexion à l'API et récupération des données

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,38 @@
-// 1. On sélectionne le bouton dans le document HTML grâce à son ID.
+// 1. Sélection du bouton
 const fetchButton = document.querySelector('#fetch-button');
 
-// 2. On ajoute un "écouteur d'événement" sur ce bouton.
-// Il va surveiller l'événement 'click' et exécuter une fonction dès que cet événement se produit.
-fetchButton.addEventListener('click', () => {
-    // 3. Pour l'instant, on se contente d'afficher un message dans la console pour vérifier que tout fonctionne.
-    console.log('Bouton cliqué ! Le JavaScript est bien connecté.');
+// 2. Définition des constantes
+const API_URL = 'https://oracles-api.sidathsoeun.fr/oracle_api.php';
+const API_KEY = 'SI_DART_Sun_api_keys_!598254741369!excalibure!paramKeysOracle!887782secretNum&5882!';
+
+// 3. Écouteur d'événement
+fetchButton.addEventListener('click', async () => {
+    
+    try {
+        // Lancement de la requête
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                "api_key": API_KEY 
+            })
+        });
+
+        // Vérification de la réponse
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+        }
+
+        // Conversion de la réponse en JSON
+        const data = await response.json();
+
+        // Affichage dans la console
+        console.log('Données reçues de l\'API :', data);
+
+    } catch (error) {
+        // Gestion de l'erreur
+        console.error('Impossible de récupérer les horoscopes:', error);
+    }
 });
